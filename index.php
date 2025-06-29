f1314967b3fdbc14e4075f7c1047540ec21c7347
HectorFunes <hectorfunes9192@unitec.edu>
2025-06-28 20:19:01 -0600

    primer commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+ <link rel="stylesheet" href="styles.css"><html>
+ <script src="https://unpkg.com/feather-icons"></script>
+ <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap" rel="stylesheet">
+ </head>
+  <body>
+    <div class="content">
+      <i data-feather="chevron-down" width="150" height="150"></i>
+      <h2>¿Eres un colaborador en estas elecciones?</h2>
+      <h1 class="accede-aqui">Accede aquí</h1>
+</br>
+
+    <h1 class="titulo-elecciones">
+      Elecciones Generales 2025
+    </h1>
+    <h1>
+    ¿Esás preparado para marcar la diferencia en tu país?
+    </h1>
+    <h1>Haz la diferencia, haz tu voto aquí</h1>
+    <h1 style="margin: 100px"></h1>
+    
+    <a class="btn-start" href="proceso.php">Comenzar</a>
+    
+    <h1 style="margin: 100px"></h1>
+    </div>
+
+    
+
+    <script>
+      feather.replace();
+    </script>
+    
+    <?php
+      //echo "hello world!";
+    ?>
+  </body>
+  <footer>
+    <h2>Heyden Aldana - Héctor Funes - Bilander Fernández</h2>
+    <h2>Copyright © todos los derechos reservados.</h2>
+    <h2>Este es un proyecto simulado y no corresponde al modelo actual del sistema de elecciones generales en Honduras </h2>
+  </footer>
+</html>
+</DOCTYPE>
