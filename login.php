5a0ea35518f6bde0f4be65cbd18f46d1d9104275
HectorFunes <hectorfunes9192@unitec.edu>
2025-06-29 12:29:14 -0600

    recuperar contraseña

--- login.php
+++ login.php
@@ -14,7 +14,7 @@
     <h1 class="titulo-elecciones" style="margin-top: 30px;margin-bottom: 40px;">
         Elecciones Generales 2025
     </h1>
-  <form action="login.php" method="post">
+  <form class="login-form" action="login.php" method="post">
 
     <?php if (isset($_GET['error'])) { ?>
       <p class="error"><?php echo $_GET['error']; ?></p>
@@ -28,8 +28,8 @@
 
     <button type="submit">Entrar</button>
   </form>
-  <h3 style="font-size:20px;font-weight:410;color: #3E64FB;">Recuperar contraseña</h3>
-  <a href="index.php" style="text-decoration:none;font-size:20px;font-weight:410;color: #000000;">Volver al menú de inicio</a>
+  <a href="recuperarContrasena.php" ><h3 class="pswd-rec">Recuperar Contraseña</h3></a>
+  <a href="index.php" class="volver-home">Volver al menú de inicio</a>
 </div>
 
 </body>
